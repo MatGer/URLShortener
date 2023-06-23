8df8f9b2c677d3ea45f258cd66c5f002ca4599e0
MatGer <34511036+MatGer@users.noreply.github.com>
2023-06-23 12:15:33 +0300

    Random string algorithm added

--- UrlShortener/Controllers/ShortController.cs
+++ UrlShortener/Controllers/ShortController.cs
@@ -19,28 +19,37 @@ namespace UrlShortener.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var links = await _linksDbContext.Links.ToListAsync();
-            var json = new
-            {
-                data = links.Select(i => new
-                {
-                    id=i.Id,
-                    lUrl=i.LongUrl,
-                    sUrl = i.ShortUrl
-                })
-            };
+            //var links = await _linksDbContext.Links.ToListAsync();
+            //var json = new
+            //{
+            //    data = links.Select(i => new
+            //    {
+            //        id=i.Id,
+            //        lUrl=i.LongUrl,
+            //        sUrl = i.ShortUrl
+            //    })
+            //};
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Shortener(Links link)
         {
-            return View();
+            var links = _linksDbContext.Links.Where(x=>x.LongUrl==link.LongUrl).FirstOrDefault();
+            if (links!=null){
+                ViewBag.link = links;
+                return RedirectToAction("Index", "Home");
+            }
+            var rand = new Random();
+            Links obj= new Links();
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            string randomString = new string(Enumerable.Repeat(chars, 10).Select(s => s[rand.Next(s.Length)]).ToArray());   //link length = 10 characters
+            obj.LongUrl = link.LongUrl;
+            obj.ShortUrl = "https://shortlink.com/" + randomString;
+            _linksDbContext.Links.Add(obj);
+            _linksDbContext.SaveChanges();
+            ViewBag.link = obj;
+            return RedirectToAction("Index", "Home");
         }
-
-        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        //public IActionResult Error()
-        //{
-        //    return NotFound();
-        //}
     }
 }
