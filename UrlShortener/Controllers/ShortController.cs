4108c6215b2f9d965e6dc906c021bffcc37e9a4b
MatGer <34511036+MatGer@users.noreply.github.com>
2023-06-26 12:30:19 +0300

    Short Url return added

--- UrlShortener/Controllers/ShortController.cs
+++ UrlShortener/Controllers/ShortController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using UrlShortener.Models;
@@ -17,39 +18,30 @@ namespace UrlShortener.Controllers
             _linksDbContext = linksDbContext;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            //var links = await _linksDbContext.Links.ToListAsync();
-            //var json = new
-            //{
-            //    data = links.Select(i => new
-            //    {
-            //        id=i.Id,
-            //        lUrl=i.LongUrl,
-            //        sUrl = i.ShortUrl
-            //    })
-            //};
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Shortener(Links link)
         {
+            var a = Request.GetDisplayUrl();
             var links = _linksDbContext.Links.Where(x=>x.LongUrl==link.LongUrl).FirstOrDefault();
             if (links!=null){
-                ViewBag.link = links;
-                return RedirectToAction("Index", "Home");
+                TempData["NewLink"] = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/{links.ShortUrl}";
+                return RedirectToAction("Index", "Short");
             }
             var rand = new Random();
             Links obj= new Links();
             const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-            string randomString = new string(Enumerable.Repeat(chars, 8).Select(s => s[rand.Next(s.Length)]).ToArray());   //link length = 8 characters
+            string randomString = new string(Enumerable.Repeat(chars, 5).Select(s => s[rand.Next(s.Length)]).ToArray());   //link length = 8 characters
             obj.LongUrl = link.LongUrl;
             obj.ShortUrl = randomString;
             _linksDbContext.Links.Add(obj);
             _linksDbContext.SaveChanges();
-            ViewBag.link = obj;
-            return RedirectToAction("Index", "Home");
+            TempData["NewLink"] = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/{obj.ShortUrl}";
+            return RedirectToAction("Index", "Short");
         }
     }
 }
